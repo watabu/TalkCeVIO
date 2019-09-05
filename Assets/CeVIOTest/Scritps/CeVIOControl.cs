486808b942c330c7d0ca2e377910e0630919be9d
watabu <4618085@ed.tus.ac.jp>
2019-09-05 18:27:08 +0900

    update
    共有・公開用

---
+++ Assets/CeVIOTest/Scritps/CeVIOControl.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+
+public class CeVIOControl : MonoBehaviour {
+    
+    private Process _process;
+	
+    // Use this for initialization
+	void Start () {
+        _process = null;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (null == _process)
+            {
+                //changeCast 名前　で変更
+                _process = new Process();
+                _process.StartInfo.FileName = Application.dataPath + "/External/CeVIO1.exe";
+                _process.StartInfo.Arguments = "changecast";
+                _process.StartInfo.Arguments = "こんにちわ さとうささらです。 はじめまして！";
+                // for Redirect
+                {
+                    _process.StartInfo.CreateNoWindow = true;
+                    _process.StartInfo.UseShellExecute = false;
+                    _process.StartInfo.RedirectStandardOutput = true;
+                }
+                // for ExitEvent
+                {
+                    _process.EnableRaisingEvents = true;
+                    _process.Exited += Process_Exited;
+                }
+                _process.Start();
+                // Redirect
+                string output = _process.StandardOutput.ReadToEnd();
+                UnityEngine.Debug.Log(output);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (null != _process)
+            {
+                if (!_process.HasExited)
+                {
+                    _process.CloseMainWindow();
+                    _process.Dispose();
+                    _process = null;
+                }
+            }
+        }
+    }
+
+    void Process_Exited(object sender, System.EventArgs e)
+    {
+        UnityEngine.Debug.Log("Process_Exited");
+        _process.Dispose();
+        _process = null;
+    }
+}
---
+++ Assets/CeVIOTest/Scritps/RequestTalkAPI.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using MiniJSON;
+using System.Diagnostics;
+
+public class RequestTalkAPI : MonoBehaviour
+{
+
+    string url = "https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk";
+    string apikey = "DZZ0ailgYJUxo9GZRokQXY6BKOsxGTdU";
+    public string query = "";
+    public Text queryText;
+    public Text replyText;
+
+    public InputField inputField;
+
+    private Process _process;
+    //  IEnumerator Start()
+
+
+    void Start()
+    {
+        _process = null;
+    }
+
+    public void Chatting()
+    {
+        StartCoroutine(ChatBot());
+    }
+
+    public void resetText()
+    {
+        inputField.text = "";
+    }
+
+    public IEnumerator ChatBot()
+    {
+        // ChatAPIに送る情報を入力
+        WWWForm form = new WWWForm();
+        query = queryText.text;
+        UnityEngine.Debug.Log("send text:" +queryText.text);
+   //     query = "おなかがすきました";
+        form.AddField("apikey", apikey);
+        form.AddField("query", query, Encoding.UTF8);
+
+        // 通信
+        using (UnityWebRequest request = UnityWebRequest.Post(url, form))
+        {
+
+            yield return request.Send();
+
+            if (request.isError)
+            {
+                UnityEngine.Debug.Log(request.error);
+            }
+            else
+            {
+                try
+                {
+                    // 取得したものをJsonで整形
+                    string itemJson = request.downloadHandler.text;
+                    UnityEngine.Debug.Log(itemJson);
+                    JsonNode jsnode = JsonNode.Parse(itemJson);
+                    // Jsonから会話部分だけ抽出してTextに代入               
+                    if (replyText.text != null)
+                    {
+                        replyText.text = jsnode["results"][0]["reply"].Get<string>();
+
+                        TalkCeVIO(jsnode["results"][0]["reply"].Get<string>());
+                    }
+                    else
+                    {
+                        TalkCeVIO("ごめんなさい、よく聞こえませんでした");
+                    }
+
+                    UnityEngine.Debug.Log("reply text: " +replyText.text);
+
+                }
+                catch (Exception e)
+                {
+                    // エラーが出たらこれがログに吐き出される
+                    UnityEngine.Debug.Log("JsonNode:" + e.Message);
+                    TalkCeVIO("ごめんなさい、よく聞こえませんでした");
+                }
+             
+            }
+        }
+    }
+
+    int ChangeCast(string name)
+    {
+
+
+        return 0;
+    }
+    void TalkCeVIO(string text)
+    {
+        if (null == _process)
+        {
+            //changeCast 名前　で変更
+            _process = new Process();
+            _process.StartInfo.FileName = Application.dataPath + "/External/CeVIO1.exe";
+            //_process.StartInfo.Arguments = "changecast";
+            _process.StartInfo.Arguments = text;
+            // for Redirect
+            {
+                _process.StartInfo.CreateNoWindow = true;
+                _process.StartInfo.UseShellExecute = false;
+                _process.StartInfo.RedirectStandardOutput = true;
+            }
+            // for ExitEvent
+            { 
+                _process.EnableRaisingEvents = true;
+                _process.Exited += Process_Exited;
+            }
+            _process.Start();
+            // Redirect
+            string output = _process.StandardOutput.ReadToEnd();
+            UnityEngine.Debug.Log(output);
+        }
+    }
+    void Process_Exited(object sender, System.EventArgs e)
+    {
+        UnityEngine.Debug.Log("Process_Exited");
+        _process.Dispose();
+        _process = null;
+    }
+}
+
